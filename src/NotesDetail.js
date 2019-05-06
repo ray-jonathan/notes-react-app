800e2b841c2869cf281109250e692a699642db4d
Jonathan Ray <jonathanray17@gmail.com>
2019-05-06 11:52:28 -0400

    adds textarea to NotesDetail

--- src/NotesDetail.js
+++ src/NotesDetail.js
@@ -1,18 +1,25 @@
 import React from 'react';
+
+function NotesEditor({text}){
+    return(
+        <textarea value={text}></textarea> 
+    );
+}
+
 export default class NotesDetail extends React.Component{
     constructor(props){
         super(props);
-
         this.state = {
-
+            isEditing: true
         };
     }
     render(){
         const {note} = this.props;
+        const {isEditing} = this.state;
         return(
         <div>
             <h2>{note.title}</h2> 
-            {note.text}
+            {isEditing? <NotesEditor text={note.text} /> : note.text }
         </div>
         )
     }
